9dcc62def15905990000499ea6428ff6e08f76be
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-08-24 16:41:02 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "C:\Users\ayaser\workspace_v10\Testato\program\Application.h"
+#include "C:\Users\ayaser\Documents\TM4C123GH6PM.h"
+#include <stdint.h>
+#include <stdlib.h>
+int main() {
+    application();
+
+}
