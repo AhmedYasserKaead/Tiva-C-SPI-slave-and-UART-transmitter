17e7bdffb32d09f9972544e6d0d37aaa9f14cd1d
Ahmed Yasser Kamal Kaead <85291999+AhmedYasserKaead@users.noreply.github.com>
2021-08-24 16:36:02 +0200

    Add files via upload

---
+++ Program/Application.c
@@ -0,0 +1,18 @@
+#include "Application.h"
+#include "C:\Users\ayaser\workspace_v10\Testato\mCAL\UART.h"
+#include "C:\Users\ayaser\workspace_v10\Testato\mCAL\Timer.h"
+#include "C:\Users\ayaser\workspace_v10\Testato\mCAL\SPI.h"
+#include "C:\Users\ayaser\workspace_v10\Testato\HAL\LED_Control.h"
+
+void application()
+{
+    UART_transmit();
+    SPI_slave();
+    SW_ini();
+    while(1)
+    {
+        UART_TX();
+        SPI_RX();
+    }
+
+}
---
+++ Program/Application.h
@@ -0,0 +1,14 @@
+/*
+ * Application.h
+ *
+ *  Created on: Aug 24, 2021
+ *      Author: ayaser
+ */
+
+#ifndef PROGRAM_APPLICATION_H_
+#define PROGRAM_APPLICATION_H_
+
+void application();
+
+
+#endif /* PROGRAM_APPLICATION_H_ */
