58ca6e33aa4aa6b1fde0058d97745fc4032a85c3
omarN1331 <63172149+omarN1331@users.noreply.github.com>
2021-08-29 12:25:59 +0200

    Update PORTAheader.h

--- mCAL/PORTAheader.h
+++ mCAL/PORTAheader.h
@@ -11,10 +11,10 @@
 #define DEN_Offset		0x51C
 
 
-#define AFSEL_A (*((volatile u32*)(portA + AFSEL_Offset)))
-#define DIR_A (*((volatile u32*)(portA + DIR_Offset)))
-#define DATA_A (*((volatile u32*)(portA + DATA_Offset)))
-#define PCTL_A (*((volatile u32*)(portA + PCTL_Offset)))
-#define DEN_A (*((volatile u32*)(portA + DEN_Offset)))
+#define AFSEL->A (*((volatile u32*)(portA + AFSEL_Offset)))
+#define DIR->A (*((volatile u32*)(portA + DIR_Offset)))
+#define DATA->A (*((volatile u32*)(portA + DATA_Offset)))
+#define PCTL->A (*((volatile u32*)(portA + PCTL_Offset)))
+#define DEN->A (*((volatile u32*)(portA + DEN_Offset)))
 
 
